d0f3d6ce44f5c7b58ecb725a09515212919365fe
Thaissa Carrafa <thaissacarrafa@gmail.com>
2021-10-06 21:40:17 -0300

    Adding Exercises Lampada

---
+++ src/main/java/ExercicioLampada.java
@@ -0,0 +1,15 @@
+public class ExercicioLampada {
+
+    public static void main(String[] args) {
+
+
+        Lampada lampada = new Lampada();// Classe + nome da classe em minúsculo = new + instanciar o objeto
+        lampada.mostrarEstado();
+        lampada.desligar();
+        lampada.mostrarEstado();
+        lampada.ligar();
+        lampada.mostrarEstado();
+        lampada.alterarEstado();
+        lampada.mostrarEstado();
+    }
+}
