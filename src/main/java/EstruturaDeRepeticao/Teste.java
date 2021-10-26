9ade8404c461a30dc3f2501cfffbee5db5c26b3c
Thaissa Carrafa <thaissacarrafa@gmail.com>
2021-10-26 14:47:34 -0300

    Labs

---
+++ src/main/java/EstruturaDeRepeticao/Teste.java
@@ -0,0 +1,13 @@
+package EstruturaDeRepeticao;
+
+public class Teste {
+    public static void main(String[] args) {
+        int num = 5, count = 1;
+        while(count <= 3) {
+            ++count;
+            num += count;
+        }
+        System.out.println(num);
+    }
+
+}
